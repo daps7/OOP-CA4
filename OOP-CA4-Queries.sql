7c611e05614a0cbd1d219103fb394725743b7377
daps7 <160857914+daps7@users.noreply.github.com>
2025-02-11 09:33:39 +0000

    Update OOP-CA4-Queries.sql

--- OOP-CA4-Queries.sql
+++ OOP-CA4-Queries.sql
@@ -3,14 +3,14 @@ USE expense_record;
 CREATE TABLE expenses (
     expenseID INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR() NOT NULL,
-    category VARCHAR(255) NOT NULL,
+    category VARCHAR(50) NOT NULL,
     amount DOUBLE NOT NULL,
     dateIncurred DATE NOT NULL
 );
 
 CREATE TABLE income (
     incomeID INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
+    title VARCHAR(50) NOT NULL,
     amount DOUBLE NOT NULL,
     dateEarned DATE NOT NULL
 );
