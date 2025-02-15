afdb123bb22cc12d10eab91c4e6ff4bde0a991c7
daps7 <160857914+daps7@users.noreply.github.com>
2025-02-15 15:15:22 +0000

    Add files via upload

--- OOP-CA4-Queries.sql
+++ OOP-CA4-Queries.sql
@@ -1,26 +1,16 @@
-CREATE DATABASE expense_record; --Database Creation
+CREATE DATABASE expense_record; --Creating Database
 USE expense_record;
---Creation of tables (income and expense)
+CREATE TABLE expenses (
+    expenseID INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR() NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    amount DOUBLE NOT NULL,
+    dateIncurred DATE NOT NULL
+);
+
 CREATE TABLE income (
     incomeID INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     amount DOUBLE NOT NULL,
     dateEarned DATE NOT NULL
 );
-
-CREATE TABLE expenses (
-    expenseID INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    category VARCHAR(255) NOT NULL,
-    amount DOUBLE NOT NULL,
-    dateIncurred DATE NOT NULL
-);
-
--- Sample Data
-INSERT INTO income (title, amount, dateEarned) VALUES 
-('Babysitting', 60, '2025-01-05'),
-('Bar work', 100, '2025-01-07');
-
-INSERT INTO expenses (title, category, amount, dateIncurred) VALUES 
-('Weekly shop', 'Groceries', 47.50, '2025-01-07'),
-('Gym membership', 'Fitness', 30, '2025-01-09');
